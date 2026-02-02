bbb853e2aa5201ca9f611fa7bba06010af7e82ce
Eloy Martínez Cuesta <eloy@eloymartinezcuesta.com>
2026-02-02 21:07:49 +0100

    Add simple local test script

---
+++ test-local.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Simple test from server to API
+
+curl -H "X-API-Key: yg22e0mHH8y08VesvL5za1Sg81q3b7NTpuitRtWe2bs" -H "User-Agent: Mozilla/5.0" http://127.0.0.1:8000/api/productos/?limit=1
